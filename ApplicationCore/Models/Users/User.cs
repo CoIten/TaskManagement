0b2c50faf505d3b641997c76f8c44a7f1e41dc09
Sandro Rusitashvili <rusitashvilisandro@gmail.com>
2023-06-12 02:07:40 +0400

    set up a checkpoint

--- ApplicationCore/Models/Users/User.cs
+++ ApplicationCore/Models/Users/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +10,41 @@ namespace ApplicationCore.Models.Users
 {
     public class User
     {
+        [Required]
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [RegularExpression("^[^0-9]*$")]
+        [Column(TypeName = "nvarchar(100)")]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [RegularExpression("^[^0-9]*$")]
+        [Column(TypeName = "nvarchar(100)")]
         public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Column(TypeName = "nvarchar(100)")]
         public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.EmailAddress)]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=.*[a-zA-Z]).{8,}$")]
+        [DataType(DataType.Password)]
+        [NotMapped]
+        public string Password { get; set; }
+
         public string PasswordSalt { get; set; }
         public string HashedPassword { get; set; }
-        public string Email { get; set; }
+
     }
 }
--- ApplicationCore/Models/Users/UserPost.cs
+++ ApplicationCore/Models/Users/UserPost.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +12,30 @@ namespace ApplicationCore.Models.Users
     public class UserPost
     {
         [Required]
+        [StringLength(100)]
+        [RegularExpression("^[^0-9]*$")]
+        [Column(TypeName = "nvarchar(100)")]
         public string FirstName { get; set; }
+
         [Required]
+        [StringLength(100)]
+        [RegularExpression("^[^0-9]*$")]
+        [Column(TypeName = "nvarchar(100)")]
         public string LastName { get; set; }
-        //public ICollection<UserRole>? UserRoles { get; set; }
+
         [Required]
+        [StringLength(100)]
+        [Column(TypeName = "nvarchar(100)")]
         public string UserName { get; set; }
+
         [Required]
+        [StringLength(100)]
         [DataType(DataType.EmailAddress)]
+        [Column(TypeName = "nvarchar(100)")]
         public string Email { get; set; }
+
         [Required]
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=.*[a-zA-Z]).{8,}$")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
