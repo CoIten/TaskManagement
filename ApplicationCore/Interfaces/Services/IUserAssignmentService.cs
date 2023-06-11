cff1be0733a0067627902fc6cb8370c3bb73b7c6
Sandro Rusitashvili <rusitashvilisandro@gmail.com>
2023-06-11 19:21:54 +0400

    introduce UserAssignment

---
+++ ApplicationCore/Interfaces/Services/IUserAssignmentService.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationCore.Models.Assignment;
+using ApplicationCore.Models.UserAssignments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces.Services
+{
+    public interface IUserAssignmentService
+    {
+        public Task<UserAssignment> AssignUserToAssignment(int userId, int assignmentId);
+        public Task RemoveAssignmentFromUser(int userId, int assignmentId);
+        public Task DeleteUserAssignment(int userId, int assignmentId);
+    }
+}
---
+++ ApplicationCore/Models/UserAssignments/UserAssignment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Models.UserAssignments
+{
+    public class UserAssignment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int AssignmentId { get; set; }
+        public string Status { get; set; }
+    }
+}
